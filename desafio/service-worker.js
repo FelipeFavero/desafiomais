60d46d929d7dd811a9938190b9272223f1f4e9c5
FelipeFavero <44851081+FelipeFavero@users.noreply.github.com>
2018-11-09 21:19:08 -0200

    Upload
    okok

---
+++ desafio/service-worker.js
@@ -0,0 +1,47 @@
+var files = [
+    "/desafio/index.html",
+    "/desafio/manifest.json",
+    "/desafio/estilo.css",
+    "/desafio/img/face.png",
+    "/desafio/img/Git.png",
+    "/desafio/img/linkedin.png",
+    "/desafio/img/perfil.png"
+  ];
+  // dev only
+  if (typeof files == 'undefined') {
+    var files = [];
+  } else {
+    files.push('./');
+  }
+  
+  var CACHE_NAME = 'feijao-v1';
+  
+  self.addEventListener('activate', function(event) {
+    console.log('[SW] Activate');
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (CACHE_NAME.indexOf(cacheName) == -1) {
+              console.log('[SW] Delete cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
+  self.addEventListener('install', function(event){
+    console.log('[SW] Install');
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return Promise.all(
+            files.map(function(file){
+                return cache.add(file);
+            })
+        );
+      })
+    );
+  });
+  
